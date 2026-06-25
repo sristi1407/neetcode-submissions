f851239a4596482335170d5bf4408b6a6cd7348b
sristi1407 <66542493+sristi1407@users.noreply.github.com>
2026-06-25 14:13:48 -0400

    Add: add-binary - submission-0

---
+++ Data Structures & Algorithms/add-binary/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num_a = int(a,2)
+        num_b = int(b,2)
+
+        total = num_a + num_b 
+
+        return bin(total)[2:]
